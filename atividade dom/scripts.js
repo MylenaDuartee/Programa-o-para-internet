4ddb37e04daa497c4a9fec859901b4fbcfd8b566
MylenaDuartee <mylenaduarte0353@gmail.com>
2026-04-18 05:06:16 -0300

    organizando arquivos na pasta atividade dom

--- atividade dom/scripts.js
+++ atividade dom/scripts.js
@@ -54,6 +54,85 @@
         paragrafo.textContent = "";
     });
 
+    //Questão 6
+    function mudarparagrafo(){
+        let elemento = getById("mudarFormato");
+        elemento.style.color = "white";
+        elemento.style.background = "#A47BAB";
+        elemento.style.borderRadius = "12px";
+        elemento.style.padding = "12px 18px";
+        elemento.style.margin = "12px 0";
+        elemento.style.maxWidth = "400px";
+        elemento.style.textAlign = "center";
+    };
+
+    //Questão 7
+    let maiusculo = getById("botaoMaiusculo");
+    let inputMaiusculo = getById("textoMinusculo");
+    let resultadoM = getById("resultadoMaiusculo");
+
+    maiusculo.addEventListener("click", function(){
+        let textoM = inputMaiusculo.value;
+        let textoCerto = textoM.toUpperCase();
+        resultadoM.innerText = textoCerto;
+    })
+
+    //Questão 8
+    function mudarBody() {
+        document.body.style.background = "black";
+        document.body.style.color = "white";
+    }
+    function corBodyOri() {
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+    }
+
+    //Questão 9
+    function aumentarTam() {
+        document.body.style.fontSize = "5rem";
+    }
+
+    function diminuirTam() {
+        document.body.style.fontSize = "3px";
+    }
+
+    //Questão 10
+    function calcular(){
+        let operador = document.querySelector('input[name="operacao"]:checked');
+        let numero1 = getById("numero1");
+        let numero2 = getById("numero2");
+        let resultadoOpera = getById("resultadoOpera");
+        let resultadoop = 0;
+        if (operador.value == "+") {
+            resultadoop = parseFloat(numero1.value) + parseFloat(numero2.value);
+        } else if (operador.value == "-") {
+            resultadoop = parseFloat(numero1.value) - parseFloat(numero2.value);
+        } else if (operador.value == "/") {
+            resultadoop = parseFloat(numero1.value) / parseFloat(numero2.value);
+        } else {
+            resultadoop = parseFloat(numero1.value) * parseFloat(numero2.value);
+        }
+
+        resultadoOpera.innerText = resultadoop
+
+    }
+
+    //Questão 11
     
+    let item = getById("item");
 
+    function inserirLi(){
+        let topico = document.createElement("li");
+        topico.innerText = item.value;
+        getById("resultadoLista").appendChild(topico);
+    }
 
+    //Questão 12
+    
+    let itemsele = getById("itemSelect");
+
+    function inserirSel(){
+        let topico = document.createElement("option");
+        topico.innerText = itemsele.value;
+        getById("resultadoSelec").appendChild(topico);
+    }
